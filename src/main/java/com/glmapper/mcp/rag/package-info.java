8e6888d2dd740f9f168a081ad1c6c98dc64b08b4
glmapper <glmapper_2018@163.com>
2025-04-07 21:00:56 +0800

    add spring ai milvus dependency

---
+++ src/main/java/com/glmapper/mcp/rag/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname package-info
+ * @Description TODO
+ * @Date 2025/4/7 20:19
+ * @Created by glmapper
+ */
+package com.glmapper.mcp.rag;
+
+// 依赖 embedding 模型进行向量化操作
+// org.springframework.ai.embedding.AbstractEmbeddingModel.dimensions(EmbeddingModel, String, String)
+// 默认使用 openai 进行向量化，因此启动时会报错
